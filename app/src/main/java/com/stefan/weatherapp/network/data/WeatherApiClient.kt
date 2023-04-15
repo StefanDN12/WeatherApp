297dc2553ee81f09859389a3db803442af27d6f4
Stefan <estefan_daniel100@hotmail.com>
2023-04-15 20:51:15 +0200

    Adds Api client implementation

--- app/src/main/java/com/stefan/weatherapp/network/data/WeatherApiClient.kt
+++ app/src/main/java/com/stefan/weatherapp/network/data/WeatherApiClient.kt
@@ -1,4 +1,11 @@
 package com.stefan.weatherapp.network.data
 
+import com.stefan.weatherapp.core.CommonHelper
+import com.stefan.weatherapp.dataclases.WeatherModel
+import retrofit2.Response
+import retrofit2.http.GET
+
 interface WeatherApiClient {
+    @GET("?unitGroup=us&include=current&key=${CommonHelper.API_KEY}&contentType=json")
+    fun getWetherMadrid(): Response<WeatherModel>
 }
