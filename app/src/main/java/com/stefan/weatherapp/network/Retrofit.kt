b7defac85681be79e2f370bd7d4f5942a74a30ec
Stefan <estefan_daniel100@hotmail.com>
2023-04-15 20:48:23 +0200

    Adds Retrofit Implementation

--- app/src/main/java/com/stefan/weatherapp/network/Retrofit.kt
+++ app/src/main/java/com/stefan/weatherapp/network/Retrofit.kt
@@ -1,4 +1,12 @@
 package com.stefan.weatherapp.network
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 object Retrofit {
+    fun getRetrofit(): Retrofit{
+        return Retrofit.Builder()
+            .baseUrl("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Madrid")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
 }
