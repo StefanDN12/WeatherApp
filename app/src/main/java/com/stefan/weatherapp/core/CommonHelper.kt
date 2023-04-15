5f3d4d0f33c08e4e91b64caa709f2b877411be12
Stefan <estefan_daniel100@hotmail.com>
2023-04-15 20:54:53 +0200

    Adds Api common helper class

--- app/src/main/java/com/stefan/weatherapp/core/CommonHelper.kt
+++ app/src/main/java/com/stefan/weatherapp/core/CommonHelper.kt
@@ -1,4 +1,11 @@
 package com.stefan.weatherapp.core
 
+import com.stefan.weatherapp.BuildConfig
+
+
 class CommonHelper {
+
+    companion object{
+        const val API_KEY:String = BuildConfig.API_KEY;
+    }
 }
